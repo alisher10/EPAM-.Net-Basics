380b36a3fb7bf7a8948fd8bc0c03378bf71e5387
Alisher Khamidov <85631085+alisher10@users.noreply.github.com>
2022-12-12 23:06:50 +0500

    Add files via upload

--- task2/Task2.cs
+++ task2/Task2.cs
@@ -1,14 +1,16 @@
 ﻿//Create a program that takes an integer in decimal, and a new base number system (from 2 to 20) from the command line. 
 //Print the original number converted to this system to the console.
-
 using System;
+using System.Collections.Generic;
 
-class Test {
-    static void Main() {
+class Test
+{
+    static void Main()
+    {
 
         int num, baseNum;
         string userInput;
-        
+
         Console.WriteLine("Enter number: ");
         userInput = Console.ReadLine();
         num = Convert.ToInt32(userInput);
@@ -20,18 +22,20 @@ class Test {
 
         List<int> remainders = new List<int>();
 
-        while (num != 0) {
+        while (num != 0)
+        {
             int remainder;
             remainder = num % baseNum;
-            remainders.Add(remainder);
-            remainder = num / baseNum;
+            remainders.Add(Convert.ToInt32(remainder));
+            num = num / baseNum;
         }
         remainders.Reverse();
 
         Console.Write("Answer is: ");
-        foreach(var i in remainders) {
+        foreach (var i in remainders)
+        {
             Console.Write(i);
         }
-        
+
     }
 }
