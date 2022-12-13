48c759ff9475190f9d534c66e145d9949303950f
Alisher Khamidov <85631085+alisher10@users.noreply.github.com>
2022-12-13 22:34:54 +0500

    XML Task

---
+++ XMLTask/Transmission.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Xml.Serialization;
+using XmlTask;
+
+namespace XmlTask
+{
+    [XmlInclude(typeof(Engine))]
+    [XmlInclude(typeof(Chassis))]
+    [XmlInclude(typeof(Transmission))]
+
+    [Serializable]
+    public class Transmission : Vehicle
+    {
+        public int NumberOfGears { get; set; }
+        public string Manufacturer { get; set; }
+        public string Type { get; set; }
+    }
+}
