6c2158a526d5b62668850eacfad703eed3cf34f8
Alisher Khamidov <85631085+alisher10@users.noreply.github.com>
2022-12-14 23:07:41 +0500

    Maximum Number of unequal characters task

---
+++ MaxNumberOfUnequalChars/MaxNumberOfUnequalChars/MaxNumberOfUnequalChars.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MaxNumberOfUnequalChars
+{
+    static void Main()
+    {
+        List<char> letter = new List<char>();
+        string test = "EPAM";
+        foreach (var i in test)
+        {
+            if (letter.Contains(i))
+            {
+                break;
+            }
+            else
+            {
+                letter.Add(i);
+            }
+        }
+
+        Console.WriteLine(letter.Count);
+
+
+    }
+}
